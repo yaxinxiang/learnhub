59bb1b4487d3d14316327cea740f3beb9cfdacfc
yaxinxiang <1010234469@qq.com>
2022-04-16 09:04:37 +0800

    modify

--- src/designpatterns/ceratepatterns/singleton/SingleObj.java
+++ src/designpatterns/ceratepatterns/singleton/SingleObj.java
@@ -12,7 +12,7 @@ public class SingleObj {
 
     /**
      * 这里是线程不安全的，可以在方法上面添加关键字synchronized使其线程安全，但效率降低
-     * @return
+     * @return SingleObj
      */
     public static SingleObj getInstance(){
         if(singleObj == null){
